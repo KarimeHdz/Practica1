99d6f0446b11aeb52fbbb2df77ce543d8367fff2
KarimeHdz <karimevheri@gmail.com>
2025-02-11 12:29:55 -0600

    app Base

--- app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -22,26 +22,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Ejercicio1Theme {
-                Content()
+        setContent{
+        Ejercicio1Theme {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
 }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
-fun Content() {
-    Text(
-        text = "Hola Jetpack Composable",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding()
-            .background(Color.Green)
-    )
+fun GreetingPreview() {
+    Ejercicio1Theme {
+        Greeting("Android")
+    }
 }
 
