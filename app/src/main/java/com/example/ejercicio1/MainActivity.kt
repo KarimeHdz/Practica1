ddf3a7cbcb7b680293dda714056550a814514299
KarimeHdz <karimevheri@gmail.com>
2025-02-11 13:52:14 -0600

    Clase 11 de febrero 2025

--- app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -17,14 +23,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             Ejercicio1Theme {
-                Content()
+                GreetingPreview()
             }
         }
     }
@@ -32,16 +41,54 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Content() {
-    Text(
-        text = "Hola Jetpack Composable",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding()
-            .background(Color.Green)
-    )
+fun GreetingPreview() {
+    Column {
+        //TextFieldSencillo()
+        TextFieldPlaceHolder()
+        TextFieldKeyboard()
+    }
 }
 
+@Composable
+fun TextFieldSencillo(){
+    var text by remember {mutableStateOf("")}
+    Column{
+        TextField(
+            value = text,
+            onValueChange = {newText -> text = newText},
+            label = { Text("introduce tu nombre") }
+
+        )
+        Text(text = "Tu nombre es:  $text")
+    }
+
+}
+
+@Composable
+fun TextFieldPlaceHolder(){
+    var text by remember {mutableStateOf("")}
+    Column{
+        TextField(
+            value = text,
+            onValueChange = {newText -> text = newText},
+            label = { Text("Nombre") },
+            placeholder = { Text("introduce tu nombre")}
+        )
+    }
+
+}
+
+@Composable
+fun TextFieldKeyboard(){
+    var text by remember {mutableStateOf("")}
+    Column{
+        TextField(
+            value = text,
+            onValueChange = {newText -> text = newText},
+            label = { Text("Numero") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text("introduce tu numero: $text")
+    }
+
+}
