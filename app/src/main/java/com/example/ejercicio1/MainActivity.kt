a232c7e110f060d1e74727d779ca7ab928d22181
KarimeHdz <karimevheri@gmail.com>
2025-02-18 23:58:36 -0600

    Clase 18 de febrero 2025 Practica1

--- app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -4,12 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -17,6 +28,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Spacer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +42,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejercicio1Theme {
-                Content()
+                GreetingPreview()
             }
         }
     }
@@ -32,16 +50,66 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Content() {
-    Text(
-        text = "Hola Jetpack Composable",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
+fun GreetingPreview() {
+    Column(
         modifier = Modifier
-            .padding()
-            .background(Color.Green)
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SimpleImage()
+        Spacer(modifier = Modifier.height(16.dp))
+        TextFieldWithInputType()
+        Spacer(modifier = Modifier.height(16.dp))
+        TextFieldWithInputType2()
+        Spacer(modifier = Modifier.height(16.dp))
+        SimpleButton()
+    }
+}
+
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.fondo),
+        contentDescription = "Fondo",
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
+@Composable
+fun TextFieldWithInputType() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        label = { Text(text = "Valor 1") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { it ->
+            text = it
+        }
+
+    )
+}
+
+@Composable
+fun TextFieldWithInputType2() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        label = { Text(text = "Valor 1") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { it ->
+            text = it
+        }
+    )
+}
+@Composable
+fun SimpleButton() {
+    Button(onClick = {
+        //your onclick code here
+    }
+    ) {
+        Text(text = "Calcular")
+    }
+}
